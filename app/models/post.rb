c4321518d7e21bb98f9dafd494aec64fce15871b
1-8192 <allegranzia@gmail.com>
2019-03-18 17:59:53 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,22 @@
+require 'pry'
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :content, :length => { :minimum => 250 }
+  validates :summary, :length => { :maximum => 250 }
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction)}
+  validate :clickbait?
+
+  def clickbait?
+    if self.title.to_s.include?("Won't Believe")
+      return true
+    elsif self.title.to_s.include?("Secret")
+      return true
+    elsif self.title.to_s.include?("Top [number]")
+      return true
+    elsif self.title.to_s.include?("Guess")
+      return true
+    else
+      errors.add(:title, "not clickbait")
+    end
+  end
 end
